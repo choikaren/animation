2cbf421e673356df21c87d4f53a6135f71552119
choikaren <57500302+choikaren@users.noreply.github.com>
2021-05-04 13:00:48 -0400

    walls

--- index.js
+++ index.js
@@ -1,62 +1,120 @@
 sprite = document.getElementById("spriteCharacter");
 character = document.getElementById("character");
+upKey = document.getElementById("up");
+leftKey = document.getElementById("left");
+downKey = document.getElementById("down");
+rightKey = document.getElementById("right");
+
+canvas = document.getElementById("canvas")
+
+//divide by 12 to put in rem
+let screenWidth = window.innerWidth/16 - 6;
+
+let screenHeight = window.innerHeight/16 - 8;
+
 let x = 0;
 let y = 0;
 
-console.log('hello');
 console.log(character);
 
-animate = function () {
+moveUp = function () {
+    console.log("Up key is connected");
+    sprite.classList.add("face-up");
+    sprite.classList.remove("face-down", "face-right", "face-left");
+    y = (y - 1);
+};
+
+moveRight = function () {
+    console.log("Right key is connected");
+    sprite.classList.add("face-right");
+    sprite.classList.remove("face-down", "face-left", "face-up");
+    x = (x + 1);
+    console.log(x)
+};
+
+moveLeft = function () {
+    console.log("Left key is connected");
+    sprite.classList.add("face-left");
+    sprite.classList.remove("face-down", "face-right", "face-up");
+    x = (x - 1);
+};
+
+moveDown = function () {
+    console.log("Down key is connected");
+    sprite.classList.add("face-down");
+    sprite.classList.remove("face-right", "face-left", "face-up");
+    y = (y + 1);
+};
+
+keyboardAnimate = function () {
     sprite.classList.add("Character_animation")
-    if (event.keyCode == "38") {
-        console.log("Up key is connected");
-        sprite.classList.add("face-up");
-        sprite.classList.remove("face-down", "face-right", "face-left");
-        y = (y - 1);
-    } else if (event.keyCode == "39") {
-        console.log("Right key is connected");
-        sprite.classList.add("face-right");
-        sprite.classList.remove("face-down", "face-left", "face-up");
-        x = (x + 1);
-    } else if (event.keyCode == "37") {
-        console.log("Left key is connected");
-        sprite.classList.add("face-left");
-        sprite.classList.remove("face-down", "face-right", "face-up");
-        x = (x - 1);
-    } else if (event.keyCode == "40") {
-        console.log("Down key is connected");
-        sprite.classList.add("face-down");
-        sprite.classList.remove("face-right", "face-left", "face-up");
-        y = (y + 1);
+    if (event.keyCode == "38" || event.keyCode == "87") {
+        moveUp();
+    } else if (event.keyCode == "39" || event.keyCode == "68") {
+        moveRight();
+    } else if (event.keyCode == "37" || event.keyCode == "65") {
+        moveLeft();
+    } else if (event.keyCode == "40" || event.keyCode == "83") {
+        moveDown();
     }
 };
+
 
 walls = function () {
     if (x < 0) {
         sprite.classList.remove("Character_animation")
         x = 0;
-    } else if (x > 48) {
+    } else if (x > screenWidth) {
         sprite.classList.remove("Character_animation")
-        x = 48;
+        x = screenWidth;
     } else if (y < 0) {
         sprite.classList.remove("Character_animation")
         y = 0;
-    } else if (y > 45) {
+    } else if (y > screenHeight) {
         sprite.classList.remove("Character_animation")
-        y = 45;
+        y = screenHeight;
     }
 }
 
 document.addEventListener('keydown', function () {
     //animate
-    animate();
+    keyboardAnimate();
     //check if overboard
     walls();
 
     character.style.left = x + "rem";
     character.style.top = y + "rem";
-})
+});
 
+upKey.addEventListener('click', function () {
+    moveUp()
+    walls();
+    character.style.left = x + "rem";
+    character.style.top = y + "rem";
+});
+
+leftKey.addEventListener('click', function () {
+    moveLeft()
+    walls();
+    character.style.left = x + "rem";
+    character.style.top = y + "rem";
+});
+
+downKey.addEventListener('click', function () {
+    moveDown()
+    walls();
+    character.style.left = x + "rem";
+    character.style.top = y + "rem";
+});
+
+rightKey.addEventListener('click', function () {
+    moveRight()
+    walls();
+    character.style.left = x + "rem";
+    character.style.top = y + "rem";
+});
+
+//when arrow keys are released, stops animation
 document.addEventListener('keyup', function () {
     sprite.classList.remove("Character_animation")
 })
