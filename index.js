34f2b48176ce45c2daf0863cbe372d5d62992edc
choikaren <57500302+choikaren@users.noreply.github.com>
2021-05-07 10:40:46 -0400

    woop woop

--- index.js
+++ index.js
@@ -7,21 +7,27 @@ rightKey = document.getElementById("right");
 let screenWidth;
 let screenHeight;
 
-findScreenSize = function () {
-    //divide by 6 to put in rem
-//6 bc width of sprite is 6
-screenWidth = window.innerWidth/16 - 6;
-//8 bc height of sprite is 8
-screenHeight = window.innerHeight/16 - 8;
-};
-findScreenSize();
-
-window.addEventListener("resize", findScreenSize())
-
 let x = 0;
 let y = 0;
 
-console.log(character);
+findScreenSize = function () {
+    console.log("resize")
+    //divide by 6 to put in rem
+    //6 bc width of sprite is 6
+    screenWidth = window.innerWidth / 16 - 6;
+    //8 bc height of sprite is 8
+    screenHeight = window.innerHeight / 16 - 8;
+};
+findScreenSize();
+window.onresize = findScreenSize;
+
+addAnimation = function () {
+    sprite.classList.add("Character_animation");
+};
+
+removeAnimation = function () {
+    sprite.classList.remove("Character_animation");
+};
 
 moveUp = function () {
     console.log("Up key is connected");
@@ -53,7 +59,7 @@ moveDown = function () {
 };
 
 keyboardAnimate = function () {
-    sprite.classList.add("Character_animation")
+    addAnimation();
     if (event.keyCode == "38" || event.keyCode == "87") {
         moveUp();
     } else if (event.keyCode == "39" || event.keyCode == "68") {
@@ -82,45 +88,107 @@ walls = function () {
     }
 }
 
+updateSpritePosition = function () {
+    character.style.left = x + "rem";
+    character.style.top = y + "rem";
+}
+
 document.addEventListener('keydown', function () {
     //animate
     keyboardAnimate();
     //check if overboard
     walls();
-
-    character.style.left = x + "rem";
-    character.style.top = y + "rem";
-});
-
-upKey.addEventListener('click', function () {
-    moveUp()
-    walls();
-    character.style.left = x + "rem";
-    character.style.top = y + "rem";
-});
-
-leftKey.addEventListener('click', function () {
-    moveLeft()
-    walls();
-    character.style.left = x + "rem";
-    character.style.top = y + "rem";
-});
-
-downKey.addEventListener('click', function () {
-    moveDown()
-    walls();
-    character.style.left = x + "rem";
-    character.style.top = y + "rem";
-});
-
-rightKey.addEventListener('click', function () {
-    moveRight()
-    walls();
-    character.style.left = x + "rem";
-    character.style.top = y + "rem";
+    updateSpritePosition();
 });
 
 //when arrow keys are released, stops animation
 document.addEventListener('keyup', function () {
-    sprite.classList.remove("Character_animation")
+    removeAnimation();
 })
+
+keypadArray = [
+    {
+        key: upKey,
+        keyfunction: function () {
+            console.log("Up key is connected");
+            sprite.classList.add("face-up");
+            sprite.classList.remove("face-down", "face-right", "face-left");
+            y = (y - 1);
+        }
+    },
+    {
+        key: leftKey,
+        keyfunction: function () {
+            console.log("Left key is connected");
+            sprite.classList.add("face-left");
+            sprite.classList.remove("face-down", "face-right", "face-up");
+            x = (x - 1);
+        }
+    },
+    {
+        key: downKey,
+        keyfunction: function () {
+            console.log("Down key is connected");
+            sprite.classList.add("face-down");
+            sprite.classList.remove("face-right", "face-left", "face-up");
+            y = (y + 1);
+        }
+    },
+    { 
+        key: rightKey,
+        keyfunction: function () {
+            console.log("Right key is connected");
+            sprite.classList.add("face-right");
+            sprite.classList.remove("face-down", "face-left", "face-up");
+            x = (x + 1);
+            console.log(x)
+        }
+    }
+]
+
+keypadArray.forEach(keypad => {
+    keypad.key.addEventListener('mousedown', function () {
+        addAnimation();
+        interval = setInterval(function () {
+            keypad.keyfunction();
+            walls();
+            updateSpritePosition();
+        }, 100)
+    });
+    keypad.key.addEventListener('mouseup', function () {
+        removeAnimation();
+        clearInterval(interval);
+    });
+});
+
+// upKey.addEventListener('mousedown', function () {
+//     addAnimation();
+//     interval = setInterval(function () {
+//         moveUp()
+//         walls();
+//         updateSpritePosition();
+//     }, 100)
+// })
+
+// upKey.addEventListener('mouseup', function () {
+//     removeAnimation();
+//     clearInterval(interval);
+// })
+
+// leftKey.addEventListener('mousedown', function () {
+//     moveLeft()
+//     walls();
+//     updateSpritePosition();
+// });
+
+// downKey.addEventListener('mousedown', function () {
+//     moveDown()
+//     walls();
+//     updateSpritePosition();
+// });
+
+// rightKey.addEventListener('mousedown', function () {
+//     moveRight()
+//     walls();
+//     updateSpritePosition();
+// });
